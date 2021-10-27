95ad65cb6c345e10ca8c542d32540fab799b9fb9
LorisThierryHuetter <loristhierryhuetter@gmail.com>
2021-10-27 15:49:49 +0200

    added board

---
+++ project/chess/board.py
@@ -0,0 +1,16 @@
+import pygame
+from .constants import BAIGE, ROWS, BROWN, BLACK, WHITE, SQUARE_SIZE
+
+class Board:
+    def __init__(self):
+        self.board = [[],
+                        []]
+        self.selected_piece = None
+        self.black_left = self.white_left = 16
+        
+    def draw_squares(self, win):
+        win.fill(BROWN)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, BAIGE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
